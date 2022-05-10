c876e782d2300d225b892b1a1d09b861b0c556af
Andres <bravor.andres24@gmail.com>
2022-05-09 23:35:39 -0500

    aun no funcionan tamaño habitula y variable

--- resolverSudoku.cpp
+++ resolverSudoku.cpp
@@ -3,34 +3,95 @@
 #include <string.h>
 #include <stdlib.h>
 
-int valor(int x){
-	if(x>0)
-		return x;
-	else
-		return -x;
-}
 
-bool pista(int x){
-	if(x>0)
-		return false;
-	else
-		return true;
-}
+typedef struct {
+	int valor;
+	bool esPista;
+}Dato;
+
+typedef struct posiciones{
+	int px;
+	int py;
+}Posicion;
 
 
 
-void imprimirSudoku(int mat[][9]){
+void imprimirSudoku(Dato tablero[][9]){
 	for(int i=0;i<9;i++){
-		if(i==3 || i== 6){
-			printf("----------------------\n");
-		}
+		if(i==3 || i==6)
+		    printf("------------------------\n");
 		for(int j=0;j<9;j++){
-			if(j==3 || j==6){
-				printf(" |");
-			}
-			printf(" %d",valor(mat[i][j]));
+			if(j==3 || j==6)
+			    printf(" |");
+			printf(" %d",tablero[i][j].valor);
 		}
 		printf("\n");
+	}
+}
+
+int limite(int p){
+	int a;
+	for(a=3;a<=p;a+=3);
+	return a-3;
+}
+
+void avanzar(Dato tablero[][9],Posicion *p){
+    do{
+    	if(p->py==8){
+    		p->py=0;
+    		p->px++;
+    		if(p->px==9)
+    		    return;
+		}else
+		    p->py++;	
+	}while(tablero[p->px][p->py].esPista);
+}
+
+void retroceder(Dato tablero[][9],Posicion *p){
+	do{
+		if(p->py==0){
+			p->py=8;
+			p->px--;
+			if(p->px==-1)
+			    return;
+		}else
+		    p->py--;
+	}while(tablero[p->px][p->py].esPista);
+}
+
+bool validar(Posicion P,Dato tablero[][9],int x){
+	for (int i=0;i<9;i++)
+		if(tablero[P.px][i].valor==x || tablero[i][P.py].valor==x)
+			return true;
+	int limX=limite(P.px);
+	int limY=limite(P.py);
+	for(int i=limX;i<limX+3;i++)
+	    for(int j=limY;j<limY+3;j++)
+	        if(tablero[i][j].valor==x)
+	            return false;
+	return true;
+}
+
+void llenarSudoku(Dato tablero[][9]){
+	Posicion P;
+	P.px=P.py=0;
+	int x=1;
+	while(P.px<9){
+		if(x>9){
+			tablero[P.px][P.py].valor=0;
+			retroceder(tablero,&P);
+			if(P.px==9)
+				return;
+			x=tablero[P.px][P.py].valor;
+			x++;
+			tablero[P.px][P.py].valor=0;
+		}else if(validar(P,tablero,x)){
+			tablero[P.px][P.py].valor=x;
+			avanzar(tablero,&P);
+			if(P.px=9);
+			x=0;
+		}else
+			x++;
 	}
 }
 
@@ -41,133 +102,6 @@ void mat0x0(int mat[][9]){
 		}
 	}
 }
-
-int limite(int x){
-    if(x<3)
-        return 0;
-    else if(x<6)
-        return 3;
-    else
-        return 6;
-	
-}
-
-bool acaja(int mat[][9],int fila,int columna,int x){
-	for(int i=limite(fila);i<limite(fila)+3;i++){
-		for(int j=limite(columna);j<limite(columna)+3;j++){
-			if(x==valor(mat[i][j])){
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-
-bool afila(int mat[],int x){
-	for(int i=0;i<9;i++){
-		if(x==valor(mat[i])){
-			return false;
-		}
-	}
-	return true;
-}
-
-bool acolumna(int mat[][9],int columna,int x){
-	for(int i=0;i<9;i++){
-		if(x==valor(mat[i][columna])){
-			return false;
-		}
-	}
-	return true;
-}
-
-
-bool encajaNumero(int mat[][9],int fila,int columna,int x){
-	return((acaja(mat,fila,columna,x)) && (afila(mat[fila],x)) && (acolumna(mat,columna,x)));
-}
-
-void avanza(int &fila,int &columna,int mat[][9]){
-	do{
-		if(columna==8){
-			columna=0;
-			fila++;
-			if(fila==9)
-			    return;
-		}else{
-			columna++;
-			
-		}
-	}while(!pista(mat[fila][columna]));
-}
-	
-void retrocede(int &fila,int &columna,int mat[][9]){
-	do{
-		if(columna==0){
-			columna=8;
-			fila--;
-			if(fila==-1)
-			    return;
-		}else{
-			columna--;
-		}
-	}while(!pista(mat[fila][columna]));
-}
-
-
-bool esSudoku(int mat[][9]){
-	for(int i=0;i<9;i++){
-		for(int j=0;j<9;j++){
-			if(mat[i][j] !=0){
-				if(encajaNumero(mat,i,j,mat[i][j])){
-					return false;
-				}
-			}
-		}
-	}
-	return true;
-}
-
-
-void llenarSudoku(int mat[][9]){
-	int fila=0,columna=0;
-	int x=9;
-	if(pista(mat[0][0])){
-		avanza(fila,columna,mat);
-	}
-	while(fila<9){
-	   // printf("%d\n",y++);
-		if(x==0){
-		
-			mat[fila][columna]=0;
-			retrocede(fila,columna,mat);
-			if(fila==-1){
-			    printf("no tiene solucion \n\n\n");
-				getchar();
-				return;		
-			}
-			x=valor(mat[fila][columna]);
-			x--;
-		}else if(encajaNumero(mat,fila,columna,x)){
-		
-			mat[fila][columna]=x;
-			
-			x=9;
-			if(fila==9){
-				return;
-			}
-		}else{
-		
-			x--;
-		}
-		imprimirSudoku(mat);
-		printf("\n\n\n\n");
-		getche();
-		system("cls");
-	}
-	
-}
-
 
 
 bool isNumber(char x){
@@ -211,30 +145,24 @@ int aNumero(char x){
 	return x-48;
 }
 
-void transformar(int mat[][9],char matAux[][9]){
+void transformar(Dato mat[][9],char matAux[][9]){
 	int x;
 	for(int i=0;i<9;i++){
 		for(int j=0;j<9;j++){
 			x=aNumero(matAux[i][j]);
 			if(x==0){
-				mat[i][j]=x;
+				mat[i][j].valor=x;
+				mat[i][j].esPista=false;
 			}else{
-				mat[i][j]=(-x);
+				mat[i][j].valor=(-x);
+				mat[i][j].esPista=true;
 			}
 		}
 	}
 }
 
-void Sudoku(int mat[][9]){
-	char matAux[9][9];
-//	leerMatriz(matAux);
-	mat0x0(mat);
-//	transformar(mat,matAux);
-//	imprimirSudoku(mat);
-	llenarSudoku(mat);
-	printf("\n\nsudoku resulto\n\n");
-	imprimirSudoku(mat);
-}
+
+
 
 
 
@@ -243,7 +171,7 @@ void inicio(int mat[][9]){
 	int t;
 	while(band){
 	    printf("      SUDOKU   \n ");
-	    printf("1:resolber Sudoku");
+	    printf("1:resolver Sudoku");
 	    printf("2:Salir");
 	    
 	    
@@ -252,20 +180,15 @@ void inicio(int mat[][9]){
 }
 
 int main(void){
-   int mat[9][9]={{  -1, 0, 0,  -1, 0, 0,  0, 0, 0  },{
-	                 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-				     0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-						
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-					
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  },{
-					 0, 0, 0,  0, 0, 0,  0, 0, 0  }};  
+    
    
-   mat0x0(mat);
-   Sudoku(mat);
+   char matriz[9][9];
+   leerMatriz(matriz);
+   Dato tablero[9][9];
+   transformar(tablero,matriz);
+   llenarSudoku(tablero);
+   printf("\n\n");
+   imprimirSudoku(tablero);
 }
   
    
